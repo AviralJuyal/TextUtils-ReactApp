4328fd4af6040038c61f8c39c8d2091971e8fc8a
aviral juyal <aviraljuyal@gmail.com>
2022-06-20 07:20:17 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import { useState } from 'react';
+import './App.css';
+import Navbar from './components/Navbar';
+import Textarea from './components/TextArea';
+import React from 'react';
+import Alert from './components/Alert';
+
+
+function App() {
+  const [mode, setmode] = useState('light');
+  const [alert, setalert] = useState(null);
+
+  const showAlert = (message , type)=>{
+    setalert({
+      msg : message,
+      type : type
+    });
+
+    setTimeout(() => {
+      setalert(null);
+    }, 1500);
+  }
+
+  const toggleMode = ()=>{
+    if(mode === 'light'){
+      setmode('dark');
+      document.body.style.backgroundColor = '#333333';
+      showAlert('Dark Mode is Enabled !' , 'success');
+    }
+    else{
+      setmode('light');
+      document.body.style.backgroundColor = 'white';
+      showAlert('Light Mode is Enabled !' , 'success');
+
+    }
+  }
+
+  return (
+    <>
+    {/* <Router> */}
+
+    <Navbar title="TextUtils" about = "About Us" mode={mode} toggleMode = {toggleMode}></Navbar>
+      <Alert alert = {alert}></Alert>
+      <div className="container my-3">
+      <Textarea showAlert= {showAlert} heading ="Enter Your Text Here" mode={mode}></Textarea>
+      {/* <Routes> */}
+        {/* <Route exact path="/" element={  } /> */}
+        {/* <Route exact path="about" element={ <AboutUs/> } /> */}
+      {/* </Routes> */}
+      </div>
+
+    {/* </Router> */}
+    </>
+  );
+}
+
+
+export default App;
---
+++ src/components/TextArea.js
@@ -0,0 +1,122 @@
+import React, {useState} from 'react'
+
+export default function TextArea(props) {
+  const [text, setText] = useState('');
+  
+
+  const upperCaseClick = ()=>{
+    // console.log("upper case");
+    if(text===''){
+        props.showAlert('Empty !! Cannot convert to upper case' , 'danger');
+
+    }
+    else{
+
+        props.showAlert('Converted to Upper case' , 'success');
+        const newText = text.toUpperCase();
+        setText(newText);
+    }
+  }
+  
+  const lowerCaseClick = ()=>{ 
+    if(text === ''){
+        props.showAlert('Empty !! Cannot converted to Lower case' , 'danger');
+
+    }
+    else{
+
+        props.showAlert('Converted to Lower case' , 'success');
+        const newText = text.toLowerCase();
+        setText(newText);
+    }
+  }
+
+  const clearClick = ()=>{
+      // console.log("upper case");
+    props.showAlert('Cleared Text !!', 'success');
+    
+    const newText = '';
+    setText(newText);
+  }
+  
+  let i=0;
+  const fontChangerClick = ()=>{
+    if(text === ''){
+        props.showAlert('Empty ! Cannot change font' , 'danger');
+        return;
+    }
+    // let i = Math.floor(Math.random()*fonts.length);
+    const fonts = ['Comic Sans MS','Tahoma' , 'Arial Black' ,'Times New Roman'];
+    document.getElementById('textInput').style.fontFamily= `${fonts[i]}`;
+    // console.log(fonts[i]);
+    if(i<fonts.length-1)
+    i++;
+    else
+    i=0;
+    // props.showAlert('Font Changed !' , 'success');
+    // setText(newText);
+  }
+//   let size = 18;
+//   const fontSizeIncClick = ()=>{
+//     document.getElementById('textInput').style.fontSize = `${size}px`;
+//     size+=2;
+//     console.log(size);
+//   }
+//   const fontSizeDecClick = ()=>{
+//     document.getElementById('textInput').style.fontSize = `${size}px`;
+//     size-=2;
+//     console.log(size);
+//   }
+
+  const copyTextClick = ()=>{
+    props.showAlert('Copied Text !!' , 'success');
+
+    const copyText = document.getElementById('textInput');
+    copyText.select();
+    navigator.clipboard.writeText(copyText.value);
+    // console.log(copyText.value);
+  }
+
+  const extraSpaceClick = ()=>{
+    props.showAlert('Removed extra spaces !', 'success');
+    let newText = text.split(/[ ]+/);
+    setText(newText.join(" "))
+  }
+
+  const onChangeFunc = (event)=>{
+    // console.log('onChange ');
+    
+    setText(event.target.value);
+  }
+
+  function countWords(str) {
+    const arr = str.split(' ');
+  
+    return arr.filter(word => word !== '').length;
+  }
+  return (
+    <>
+    <div className={`container text-${props.mode === 'dark'?'light':'dark'}`}>
+            
+        <h1>{props.heading}</h1>
+        <textarea className = {`form-control text-${props.mode === 'dark'?'light':'dark'} bg-${props.mode}`} spellCheck="true"  name="text" value={text} onChange={onChangeFunc} id="textInput" cols="100" rows="10"></textarea>
+        <button className='btn btn-primary my-2 mx-1' onClick={upperCaseClick}>To Upper Case</button>
+        <button className='btn btn-primary mx-1' onClick={lowerCaseClick}>To Lower Case</button>
+        <button className='btn btn-primary mx-1' onClick={clearClick}>Clear Text</button>
+        <button className='btn btn-primary mx-1' onClick={fontChangerClick}>Font</button>
+        {/* <button className='btn btn-primary mx-1' onClick={fontSizeIncClick}>Font Size (increase)</button> */}
+        {/* <button className='btn btn-primary mx-1' onClick={fontSizeDecClick}>Font Size (decrease)</button> */}
+        <button className='btn btn-primary mx-1' onClick={copyTextClick}>Copy Text</button>
+        <button className='btn btn-primary mx-1' onClick={extraSpaceClick}>Remove Extra Spaces</button>
+
+    </div>
+    <div className={`container my-3 text-${props.mode === 'dark'?'light':'dark'}`}>
+        <h2>Your text Summary</h2>
+        <p>{countWords(text)} words {text.length} characters</p>  
+        <h2>Preview</h2>
+        <p>{text}</p>
+    </div>
+    {/* text===''?text.split(' ').length-1:text.split(' ').length */}
+    </>
+  )
+}
